b537c695590d8f5c117d4e705b4ee5b007c879b8
Danil <danil@123e.com>
2022-12-15 08:24:32 +0200

    task 2

--- olympics.py
+++ olympics.py
@@ -1,12 +1,14 @@
 import sys
 
+
 def output_function(filename, output_string):
     if filename:
         with open(filename, 'a') as file1:
-            file1.write(output_string+"\n")
+            file1.write(output_string + "\n")
     print(output_string)
 
-def task1(filename, country, year, output):
+
+def parse_csv(filename, country, year, output, total_year):
     head = None
     is_first_line = True
     with open(filename, "r") as file:
@@ -14,14 +16,24 @@ def task1(filename, country, year, output):
         medals = 0
         country_exist = False
         olympics_held_this_year = False
+        total_year_dict = dict()
         for line in file.readlines():
-            line = line.replace('"', "")
-            data = line.strip().split(",")
+            data = line.strip().split(';')
 
             if is_first_line:
                 head = data
                 is_first_line = False
                 continue
+            if total_year:
+                if total_year == data[head.index("Year")] and data[-1] != "NA":
+                    if not data[head.index("Team")] in total_year_dict:
+                        total_year_dict[data[head.index("Team")]] = {data[-1]: 1}
+                    else:
+                        if not data[-1] in total_year_dict[data[head.index("Team")]]:
+                            total_year_dict[data[head.index("Team")]][data[-1]] = 1
+                        else:
+                            total_year_dict[data[head.index("Team")]][data[-1]] += 1
+
             if (country == data[head.index("NOC")] or country == data[head.index("Team")]):
                 country_exist = True
             if year == data[head.index("Year")]:
@@ -29,8 +41,8 @@ def task1(filename, country, year, output):
             if (country == data[head.index("NOC")] or country == data[head.index("Team")]) and year == data[
                 head.index("Year")]:
                 if first_10_medals < 10 and data[-1] != "NA":
-
-                    output_function(output, f"Name: {data[head.index('Name')]} Sport: {data[head.index('Sport')]} Medal: {data[-1]}")
+                    output_function(output,
+                                    f"Name: {data[head.index('Name')]} Sport: {data[head.index('Sport')]} Medal: {data[-1]}")
                     first_10_medals += 1
                 if data[head.index("Medal")] != "NA":
                     medals += 1
@@ -40,11 +52,17 @@ def task1(filename, country, year, output):
         if not country_exist:
             output_function(output, f"Країна {country} не існує")
         if medals:
-            output_function(output, "Загальна кількість медалей: "+str(medals))
-
-
-def task2():
-    pass
+            output_function(output, "Загальна кількість медалей: " + str(medals))
+        if not total_year_dict:
+            output_function(output, f"Тотальна кількість медалей у рік {total_year} не знайдена")
+        else:
+            total_year_output_string = ""
+            for country in total_year_dict:
+                total_year_output_string = total_year_output_string + country + " "
+                for medal in total_year_dict[country]:
+                    total_year_output_string = total_year_output_string + medal + ":" + str(total_year_dict[country][medal]) + " "
+                total_year_output_string = total_year_output_string + "\n"
+            output_function(filename, total_year_output_string)
 
 
 def main():
@@ -54,12 +72,17 @@ def main():
         country = args[3]
         year = args[4]
         try:
-            output = args[args.index("-output")+1]
+            output = args[args.index("-output") + 1]
         except:
             output = None
-        task1(filename, country, year, output)
+        try:
+            total_year = args[args.index("-total") + 1]
+        except:
+            total_year = None
+        parse_csv(filename, country, year, output, total_year)
 
 
 if __name__ == '__main__':
     main()
+
 
